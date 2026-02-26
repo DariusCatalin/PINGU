14f7c23f4b6d1cd37fc7755f00ee40e95b00446f
eloy <eloby09@alumnes.ilerna.com>
2026-02-26 18:52:33 +0100

    Creación de la clase Pinguino
    Se ha creado la clase Pinguino con todos los getters y setter y métodos necesarios .

---
+++ src/JOC_DEL_PINGU/Pinguino.java
@@ -0,0 +1,39 @@
+package JOC_DEL_PINGU;
+
+public class Pinguino extends Jugador {
+	
+private Inventario inv;
+	
+	public Pinguino (int posicion, String nombre, String color) { // Contructor Llamando al constructor de la clase Jugador
+		super(posicion, nombre, color);
+		this.inv = new Inventario();
+	}
+	
+	// Getters y Setters
+
+	public Inventario getInv() { 
+		return inv;
+	}
+
+	public void setInv(Inventario inv) {
+		this.inv = inv;
+	}
+	
+	public void gestionarBatalla() { // Gestiona el inventario del jugador
+		System.out.println("Gestionar inventario de " + p.getNombre());
+	}
+	
+	public void usarItem() { // Muestra el nombre del item utilizado
+		System.out.println("Usando el objeto: " + i.getNombre());
+	}
+	
+	public void añadirItem(Item i) { // Añade item al inventario
+		this.inv.getLista().add(i);
+	}
+	
+	public void quitarItem(Item i) { // Quita item del inventario
+		this.inv.getLista().remove(i);
+	}
+
+
+}
