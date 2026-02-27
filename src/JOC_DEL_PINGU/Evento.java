c84d9a28adbde5addc3ce4e38c8c4c02e39061dd
DariusCatalin <djdarius995@alumnes.ilerna.com>
2026-02-27 17:03:26 +0100

    Avance en la subclase Evento
    Se ha añadido los mensajes correspondientes a cada objeto, además, también se ha creado el sistema de probabilidad de que te toque X objeto. Faltaría poder añadir esos objetos al inventario, perder turnos / objetos, etc.

--- src/JOC_DEL_PINGU/Evento.java
+++ src/JOC_DEL_PINGU/Evento.java
@@ -6,11 +6,40 @@ public class Evento extends Casilla {
 	//CONSTRUCTOR
 	public Evento(int posicion) {
 		super(posicion);
-		this.eventos = eventos;
+		this.eventos = new String[] {
+				" ha obtenido un dado lento!", 
+				" se ha encontrado el objeto bola de nieve!",
+				" ha pescado un pez!",
+				" ha tenido mucha suerte! ¡Hasobtenido un dado rápido!",
+				" pierde un turno.",
+				" acaba de perder un objeto."
+		};
 	}
 	//FUNCIÓN REALIZAR ACCIÓN SI ES EVENTO
 	@Override
 	public void realizarAccion(Partida p, Jugador j) {
+		//GENERAR NÚMERO RANDOM PARA LA PROBABILIDAD
+		int i = (int) (Math.random() * 20) + 1;
+		//DADO LENTO
+		if (i <= 7) {
+			System.out.println("¡" + j.getNombre() + eventos[0]);
+		//BOLA DE NIEVE
+		} else if (i >= 8 && i <= 12) {
+			int cantidadBolas = (int) (Math.random() * 3) + 1;
+			System.out.println(j.getNombre() + eventos[1] + " Cantidad conseguida: " + cantidadBolas);
+		//PEZ
+		} else if (i >= 13 && i <= 15) {
+			System.out.println("¡" + j.getNombre() + eventos[2]);
+		//DADO RÁPIDO	
+		} else if (i == 16 || i == 17) {
+			System.out.println("¡" + j.getNombre() + eventos[3]);
+		//PERDER TURNO
+		} else if (i == 18 || i == 19) {
+			System.out.println(j.getNombre() + eventos[4]);
+		//PERDER OBJETO
+		} else {
+			System.out.println(j.getNombre() + eventos[5]);
+		}
 		
 	}
 }
