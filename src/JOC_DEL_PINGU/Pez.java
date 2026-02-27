cb484f2926f0edd7d9d6ef102de188fa7e710c19
eloy <eloby09@alumnes.ilerna.com>
2026-02-27 16:42:37 +0100

    Creación Clase Pez
    Se ha creado la clase Pez con relación extend a la clase Item, con un costructor llamando al constructor de la clase Item.

---
+++ src/JOC_DEL_PINGU/Pez.java
@@ -0,0 +1,11 @@
+package JOC_DEL_PINGU;
+
+public class Pez extends Item { // clase Pez con relación extend de clase Item
+	
+	public Pez(String nombre, int cantidad) { //Constructor Pez llamando a los atributos y constructor de la clase Item
+		super(nombre, cantidad);
+	}
+	
+	
+
+}
