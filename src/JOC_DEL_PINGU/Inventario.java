7a25e8e85d295a8b3e33083c713d1dda6fffe45f
adonis25042 <adonis25042@alumnes.ilerna.com>
2026-03-02 12:32:04 +0100

    Modificación de la clase Jugador
    He añadido el inventario.

---
+++ src/JOC_DEL_PINGU/Inventario.java
@@ -0,0 +1,8 @@
+package JOC_DEL_PINGU;
+
+public class Inventario {
+    // Constructor vacío
+    public Inventario() {
+        // Solo para que no de error en jugador
+    }
+}
--- src/JOC_DEL_PINGU/Jugador.java
+++ src/JOC_DEL_PINGU/Jugador.java
@@ -1,48 +1,41 @@
 package JOC_DEL_PINGU;
 
 public abstract class Jugador {
-	//Atributos
-	private int posicion;
-	private String nombre;
-	private String color;
 	
-	//Constructor
-	public Jugador(int posicion, String nombre, String color) {
-		this.posicion = posicion;
-		this.nombre = nombre;
-		this.color = color;
-		
-	}
-	
-	public void moverPosicion(int p) {
-		this.posicion = p;
-		System.out.println(this.nombre + "se ha movido a la posicion" + this.posicion);
-	}
-	
-	//Getters y setters
-	public int getPosicion() {
-		return posicion;	
-	}
-	
-	public void setPosicion(int posicion) {
-		this.posicion = posicion;
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-	
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	public String getColor() {
-		return color;
-	}
-	
-	public void setColor(String color) {
-		this.color = color;
-	}
-	
+ // Atributos
+ private int posicion;
+ private String nombre;
+ private String color;
+ private Inventario inventario; 
 
+ // Constructor
+ public Jugador(int posicion, String nombre, String color) {
+     this.posicion = posicion;
+     this.nombre = nombre;
+     this.color = color;
+     this.inventario = new Inventario(); 
+ }
+
+ public void moverPosicion(int p) {
+     this.posicion = p;
+ }
+
+ // --- Getters y Setters
+
+ public int getPosicion() { return posicion; }
+ public void setPosicion(int posicion) { this.posicion = posicion; }
+
+ public String getNombre() { return nombre; }
+ public void setNombre(String nombre) { this.nombre = nombre; }
+
+ public String getColor() { return color; }
+ public void setColor(String color) { this.color = color; }
+ public Inventario getInventario() {
+     return inventario;
+ }
+
+ public void setInventario(Inventario inventario) {
+     this.inventario = inventario;
+ }
+ 
 }
