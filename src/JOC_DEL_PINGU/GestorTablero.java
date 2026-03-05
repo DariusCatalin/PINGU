0adda34f7804aef7934eee13a9d98e22f22c9b7b
eloy <eloby09@alumnes.ilerna.com>
2026-03-05 16:07:34 +0100

    Continuación de GestorTablero
    Se ha modificado el método de comprobarFinTurno para que pueda comprobar si se ha llegado a la meta (>= 50) o si se acaba el turno del jugador actual.

--- src/JOC_DEL_PINGU/GestorTablero.java
+++ src/JOC_DEL_PINGU/GestorTablero.java
@@ -1,8 +1,8 @@
 package JOC_DEL_PINGU;
 
-public class GestorTablero {
+public class GestorTablero { // creamos la clase que gestiona el tablero.
 	
-	public void ejecutarCasilla(Partida partida, Pinguino p, Casilla c) {
+	public void ejecutarCasilla(Partida partida, Pinguino p, Casilla c) { // método el cual va a ejecutar la casilla donde se situa el jugador.
 		
 		System.out.println("Ejecutando la casilla para el Pingüino: " + p.getNombre());
 		
@@ -10,7 +10,32 @@ public class GestorTablero {
 		
 	}
 	
-	public void comprobarFinTurno(Partida partida) {
+	public void comprobarFinTurno(Partida partida) { //creamos el método que va a comprobar cuando finaliza el turno del jugador y si ha terminado la partida llegando a la meta.
+		
+		int jug_actual = partida.getJugadorActual();
+		Jugador jugador = partida.getJugadores().get(jug_actual);
+		
+		int meta = 50;
+		
+		if(jugador.getPosicion() >= meta) {
+			
+			System.out.println("El jugador " + jugador.getNombre() + " ha llegado a la meta.");
+			
+			partida.setGanador(jugador);
+			partida.setFinalizada(true);
+			
+		} else {
+			
+			System.out.println("Fin de turno de " + jugador.getNombre());
+			
+			int jugadores = partida.getJugadores().size();
+			int siguiente = (jug_actual + 1) % jugadores;
+			
+			partida.setJugadorActual(siguiente);
+			
+			partida.setTurnos(partida.getTurnos() + 1);
+		}
+		
 		
 	}
 
