5296b569d0e040659c3426013a259681c2c37c15
eloy <eloby09@alumnes.ilerna.com>
2026-03-05 17:17:49 +0100

    Modificación Tablero
    Se han añadido comentarios en el código.

--- src/JOC_DEL_PINGU/Tablero.java
+++ src/JOC_DEL_PINGU/Tablero.java
@@ -1,14 +1,16 @@
 package JOC_DEL_PINGU;
 import java.util.ArrayList;
-public class Tablero {
+public class Tablero { // Se Crea la clase Tablero
 	
-	private ArrayList<Casilla> casillas;
+	private ArrayList<Casilla> casillas; // Creamos ArrayList para guardar las casillas
 	
-	public Tablero() {
+	public Tablero() { // Constructor que llama al ArrayList creado
 		this.casillas = new ArrayList<>();
 	}
+	
+	//GETTERS/SETTERS
 
-	public ArrayList<Casilla> getCasillas() {
+	public ArrayList<Casilla> getCasillas() { 
 		return casillas;
 	}
 
@@ -16,7 +18,7 @@ public class Tablero {
 		this.casillas = casillas;
 	}
 	
-	public void actualizarTablero() {
+	public void actualizarTablero() { // método que actualiza el tablero.
 		System.out.println("Actualizando Tablero ... ");
 		
 		if(casillas.isEmpty()) {
